afac444017f2ce27584d905c9bb64903d92eea16
kseniya303 <47789657+kseniya303@users.noreply.github.com>
2019-10-13 13:42:56 +0300

    !
    dd

--- Program.cs
+++ Program.cs
@@ -27,44 +27,29 @@ namespace Tanki
                 pantera[i] = new Tank(rnd.Next(5, 10), rnd.Next(80, 110), rnd.Next(1, 100), rnd.Next(1, 10), rnd.Next(1, 10), "pantera");
             }
 
-            for (int i = 0; i < 3; i++)
+           for (int i = 0; i < 3; i++)
             {
-
-                Console.WriteLine("До боя:");
-                t[i].Print();
-                pantera[i].Print();
-                Tank.Winner(t[i], pantera[i]);
-                Console.WriteLine("Послк боя:");
-                t[i].Print();
-                pantera[i].Print();
+                //Console.WriteLine("До боя:");
+                //t[i].Print();
+                //pantera[i].Print();
+                //Console.WriteLine("ПослЕ боя:");
+                //t[i].Print();
+                //pantera[i].Print();
                 p[i] = t[i] * pantera[i];
                 Console.WriteLine("Победил:");
                 p[i].Print();
+                if (p[i] == p[2])
+                {
+                    if (p[0].Name == p[1].Name || p[0].Name == p[2].Name)
+                    {
+                        Console.WriteLine("Победила армия " + p[0].Name);
+                    }
+                    else
+                        Console.WriteLine("Победила армия " + p[1].Name);
+                }
                 Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine();
-            }
+            }  
 
-            //var t34List = new List<Tank>();
-            //var panteraList = new List<Tank>();
-
-                //for(int i = 0; i<3; i++)
-                //{
-                //    t34List.Add(new Tank("t34: "+i));
-                //    panteraList.Add(new Tank("pantera: " + i)); 
-                //}
-
-
-                //var armiT34 = new Armia(t34List, new TankEnumerator(t34List) );
-                //var armiPantera = new Armia(panteraList, new TankEnumerator2(panteraList));
-                //Console.WriteLine("Бой:");
-                //foreach (var tankT34 in armiT34)
-                //{
-                //    foreach(var tankPantera in armiPantera)
-                //    {  
-                //        Tank.Winner(tankT34, tankPantera);
-                //    }
-                //}
         }
     }
 }
